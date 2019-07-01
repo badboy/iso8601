625212de0e04ddf8e96746066d6d04c4e5fbe55a
Jan-Erik Rediger <janerik@fnordig.de>
2019-07-01 21:42:58 +0200

    Remove unsafe use of unsafe and instead panic

--- src/helper.rs
+++ src/helper.rs
@@ -1,12 +1,6 @@
-use std::str::{from_utf8_unchecked, FromStr};
-
-pub fn to_string(s: &[u8]) -> &str {
-    unsafe { from_utf8_unchecked(s) }
-}
-pub fn to_u32(s: &str) -> u32 {
-    FromStr::from_str(s).unwrap()
-}
+use std::str::{self, FromStr};
 
 pub fn buf_to_u32(s: &[u8]) -> u32 {
-    to_u32(to_string(s))
+    let s = str::from_utf8(s).expect("Invalid data, expected UTF-8 string");
+    FromStr::from_str(s).expect("Invalid string, expected ASCII reprensation of a number")
 }
